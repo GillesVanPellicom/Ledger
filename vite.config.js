28b2dc5c14cb1f809c253ef2732239b83badecd2
Gilles Van pellicom <gilles.van.pellicom1@gmail.com>
2026-01-21 11:51:49 +0100

    Fixed production deployment bug

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,24 @@ import react from "@vitejs/plugin-react";
 import javascriptObfuscator from "vite-plugin-javascript-obfuscator";
 
 export default defineConfig(({ mode }) => ({
+  base: './',
   plugins: [
     react(),
 
     // Only obfuscate in production builds
-    mode === "production" &&
-      javascriptObfuscator({
-        compact: true,
-        controlFlowFlattening: true,
-        controlFlowFlatteningThreshold: 0.75,
-        deadCodeInjection: true,
-        deadCodeInjectionThreshold: 0.4,
-        stringArray: true,
-        stringArrayEncoding: ["base64"],
-        stringArrayThreshold: 0.75,
-        renameGlobals: false, // IMPORTANT for React + Electron
-        sourceMap: false
-      })
+    // mode === "production" &&
+    //   javascriptObfuscator({
+    //     compact: true,
+    //     controlFlowFlattening: true,
+    //     controlFlowFlatteningThreshold: 0.75,
+    //     deadCodeInjection: true,
+    //     deadCodeInjectionThreshold: 0.4,
+    //     stringArray: true,
+    //     stringArrayEncoding: ["base64"],
+    //     stringArrayThreshold: 0.75,
+    //     renameGlobals: false, // IMPORTANT for React + Electron
+    //     sourceMap: false
+    //   })
   ].filter(Boolean),
 
   build: {
